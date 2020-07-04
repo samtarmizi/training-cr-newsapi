e820d7fa6377a51a208d04456a46b38719459a2e
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2020-07-04 16:04:37 +0800

    feat: php unit - setup and tear down

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,16 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->artisan('migrate');
+    }
+
+    protected function tearDown(): void
+    {
+        $this->artisan('config:clear');
+        parent::tearDown();
+    }
 }
